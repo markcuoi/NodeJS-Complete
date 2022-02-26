3e39f852f7257444f77d719836745bf66123af87
mark <thanhphan10397@gmail.com>
2022-02-27 00:22:01 +0700

    add delete and import data to mongodb

--- NodeJS-2/4-natours/models/tourModel.js
+++ NodeJS-2/4-natours/models/tourModel.js
@@ -5,17 +5,65 @@ const tourSchema = new mongoose.Schema({
     type: String,
     require: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
 
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must hav a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must hav a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must hav a difficulty'],
+  },
+
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
 
   price: {
     type: Number,
     require: [true, 'A tour must have a price'],
   },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'The tour must have a description'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, 'The tour must have a cover image'],
+  },
+
+  images: [String],
+
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
