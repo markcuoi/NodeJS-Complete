11e2e3c3dab484abe117fa1417f690f89bf2c6e2
mark <thanhphan10397@gmail.com>
2022-03-08 22:30:21 +0700

    basic ts class

--- Typescript-Basic/features/interfaces.ts
+++ Typescript-Basic/features/interfaces.ts
@@ -1,17 +1,24 @@
-interface Vehicle{
-  name:string;
-  year:number;
-  broken:boolean;
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
 }
 
-const oldCivic = {
-  name:'civic',
-  year:2000,
-  broken:true,
-}
+const oldCivic: Vehicle = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
 
-const printVehicle = (vehicle:Vehicle):void => {
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Name: ${vehicle.name}`)
-}
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Name: ${vehicle.name}`);
+  console.log(vehicle.summary());
+};
+
+printVehicle(oldCivic);
